9dac400c5b4dae3f7b525f7e7c8eb1932e706524
udip.bhalodiya@bacancytechnology.com <udippatel18@gmail.com>
2021-03-04 13:13:32 +0530

    changes for reg

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -14,6 +14,7 @@ class RegisterController extends BaseController
 {
 	public function register(Request $request)
 	{
+		//echo change.
 		$validator = Validator::make($request->all(), [
 			'name' => 'required',
 			'email' => 'required|email',
